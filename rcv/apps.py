96eda87c1c125b29e42f54e5d1c615a19c867d5e
Andy Situ <and.situ@gmail.com>
2017-06-07 17:36:42 -0700

    init

---
+++ rcv/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RcvConfig(AppConfig):
+    name = 'rcv'
---
+++ rcv/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class TestModel(models.Model):
+	"""
+	Just creating a model for testing purposes
+	"""
+
+	num_times = models.IntegerField()
+
+	
