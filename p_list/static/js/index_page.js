468d87baaf9539ef03bc71a2b012190f0d0fa382
Andy Situ <and.situ@gmail.com>
2017-06-19 12:25:58 -0700

    handle html changes previously

--- p_list/static/js/index_page.js
+++ p_list/static/js/index_page.js
@@ -21,40 +21,56 @@ function getCookie(name) {
     return cookieValue;
 }
 
-function testy() {
+
+
+function clicK_delete_link_response(e) {
     httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState == XMLHttpRequest.DONE) {
             if (httpRequest.status == 200) {
-                console.log(httpRequest.responseText);
+                if (httpRequest.responseText == "0") {
+                    write_message("You need to login")
+                } else {
+                    console.log(httpRequest.responseText);
+                }
             }
         }
     };
-    httpRequest.open('POST', './delete/', true);
-    httpRequest.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    httpRequest.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-    httpRequest.send()
+    if (e.target.classList.contains("add-link-delete")) {
+        httpRequest.open('POST', './delete/', true);
+        httpRequest.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        httpRequest.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+        httpRequest.send()
+    } else {
+
+    }
+    
 }
 
 
-function clicky(e) {
+function hoovering_rcv(e) {
     var link_ele = e.target;
     var rcv = link_ele.text;
-    var test_element = document.getElementById(rcv + '-test');
-    test_element.classList.remove('add-link-hide');
-    test_element.classList.add('add-link-unhide');
-    test_element.addEventListener('click', testy);
+
+    var add_link_div = document.getElementById(rcv + '-add-links-div');
+    add_link_div.classList.remove('add-link-hide');
+    add_link_div.classList.add('add-link-unhide');
+
+    for (var i = 0; i < add_link_div.children.length; i++) {
+        add_link_div.children[i].addEventListener('click', clicK_delete_link_response);
+    }
 
     var li_parent_ele = link_ele.parentElement;
     li_parent_ele.addEventListener('mouseleave', function(f) {
-        test_element.classList.remove('add-link-unhide');
-        test_element.classList.add('add-link-hide');
+        add_link_div.classList.remove('add-link-unhide');
+        add_link_div.classList.add('add-link-hide');
     });
 };
 
 for (var i = 0; i < elements_length; i++) {
-    elements[i].addEventListener('mouseenter', clicky);
+    elements[i].addEventListener('mouseenter', hoovering_rcv);
 };
+
 function write_message(msg) {
     msg_box = document.getElementById("message-box")
     msg_box.textContent = msg
