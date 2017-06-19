eb833b1b988a30250736fde143c6367241222bb9
Andy Situ <and.situ@gmail.com>
2017-06-19 12:21:52 -0700

    correct p_list link in static settings

--- p_site/settings.py
+++ p_site/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "/p_lust/static")
+    os.path.join(BASE_DIR, "/p_list/static")
 ]
 
 LOGIN_REDIRECT_URL = '/'
