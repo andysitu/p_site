c482ae0a074ece335f8f2d7a8deba0bea740f899
Andy Situ <and.situ@gmail.com>
2017-06-27 12:04:24 -0700

    update settings

--- p_site/settings.py
+++ p_site/settings.py
@@ -137,13 +137,13 @@ LOCALE_PATHS = (
 LANGUAGES = [
     ('en', gettext_lazy('English')),
     ('zh-cn', gettext_lazy('Chinese')),
-    
 ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
