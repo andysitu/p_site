571213dbca276a7051b532678d2d9d8823b0f4e0
Andy Situ <and.situ@gmail.com>
2017-06-27 12:31:24 -0700

    change static settings & staticcollect

--- p_site/settings.py
+++ p_site/settings.py
@@ -146,8 +146,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "p_site/"),
-    os.path.join(BASE_DIR, "p_list/")
+    os.path.join(BASE_DIR, "p_site/static"),
+    os.path.join(BASE_DIR, "p_list/static"),
 ]
 
 LOGIN_REDIRECT_URL = '/'
