4ae6d95c87c22d066efabc079babe677a01567eb
Andy Situ <and.situ@gmail.com>
2017-06-26 17:46:05 -0700

    Change database settings

--- p_site/settings.py
+++ p_site/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from django.utils.translation import gettext, gettext_lazy
 
+from . import pass_store
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,8 +88,12 @@ WSGI_APPLICATION = 'p_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'psite',
+        'USER': pass_store.postgres_id,
+        'PASSWORD': pass_store.postgres_pass,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
