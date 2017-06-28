0ee5cf9de202ed360b78376af2cb32a52f950baa
Andy Situ <and.situ@gmail.com>
2017-06-28 16:18:52 -0700

    minor addition of content

--- customs_list/urls.py
+++ customs_list/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^test/$', views.test),
     url(r'^upload/$', views.upload, name="upload"),
     url(r'^view_all/$', views.list_all, name="view_all"),
+    url(r'^index$', views.list_all, name="index"),
+    url(r'^$', views.list_all, name="index-blank"),
 ]
