456cada29f27652963b01d52b88d3d2b3f8bf9d1
Andy Situ <and.situ@gmail.com>
2017-07-14 15:00:03 -0700

    add list_display and list_filter to RCV admin

--- rcv_list/admin.py
+++ rcv_list/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import RCV
 
-admin.site.register(RCV)
+class RCVAdmin(admin.ModelAdmin):
+    list_display = ('rcv_number', 'correct_name', 'upload_date')
+    list_filter = ('upload_date', 'rcv_date')
+
+admin.site.register(RCV, RCVAdmin)
