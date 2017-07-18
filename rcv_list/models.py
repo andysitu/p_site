9fb4c4c845534386552731d5fdaf7e2053cbd948
Andy Situ <and.situ@gmail.com>
2017-07-18 16:12:28 -0700

    add user signal parameter

--- rcv_list/models.py
+++ rcv_list/models.py
@@ -4,6 +4,7 @@ import django
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
 from django.conf import settings
+from django.contrib.auth.models import User
 
 rcv_foldername = 'rcv'
 
@@ -18,7 +19,7 @@ class RCV(models.Model):
     def __str__(self):
         return self.rcv_number
 
-    @receiver(pre_delete)
+    @receiver(pre_delete, sender=User)
     def delete_file(sender, instance, using, **kwargs):
         try:
             filepath = os.path.join(settings.MEDIA_ROOT, rcv_foldername, instance.filename)
