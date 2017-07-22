c973eab591660673f08a6a59aa361d7320d1e6bc
Andy Situ <and.situ@gmail.com>
2017-07-21 17:21:07 -0700

    update models

--- warehouse_map/models.py
+++ warehouse_map/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class Test(models.Model):
     test_field  = models.CharField(max_length=50)
@@ -11,6 +12,9 @@ class RackLocation(models.Model):
     level = models.CharField(max_length=2)
     column = models.CharField(max_length=2)
 
+class RCV(models.Model):
+    rcv = models.CharField(max_length=20)
+
 class DataDate(models.Model):
     date = models.DateTimeField()
 
@@ -18,19 +22,19 @@ class Customer(models.Model):
     customer_code = models.IntegerField()
 
 class ItemInfo(models.Model):
-    name = models.CharField(max_length=75)
+    # Permanent item info that is the same for all items of same time
+    description = models.CharField(max_length=75)
     sku_name = models.CharField(max_length = 45)
-    sku_code = models.IntegerField()
     customer_code = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
-class ItemDateInfo(models.Model):
-    data_date = models.ForeignKey(DataDate, on_delete=models.CASCADE)
+class Item(models.Model):
+    # Info that will change constantly
+    id = models.UUIDField(primary_key=True, default =uuid.uuid4, editable=False)
     quantity = models.IntegerField()
     ship_quantity = models.IntegerField()
     rack_location = models.ManyToManyField(RackLocation)
-
-class ItemInputInfo(models.Model):
     inven_date = models.DateTimeField()
-    rcv = models.CharField(max_length=20)
-    item_date_info = models.ForeignKey(ItemDateInfo, on_delete=models.CASCADE)
+
+    data_date = models.ForeignKey(DataDate, on_delete=models.CASCADE)
+    rcv = models.ForeignKey(RCV, on_delete=models.CASCADE)
     item_info = models.ForeignKey(ItemInfo, on_delete=models.CASCADE)
