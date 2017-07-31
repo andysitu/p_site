f4f59b973fe791344eccb36d10eca038a94aa256
Andy Situ <and.situ@gmail.com>
2017-07-31 11:18:24 -0700

    edit model

--- warehouse_data/models.py
+++ warehouse_data/models.py
@@ -155,20 +155,8 @@ def populate_v_rack():
                              )
             r.save()
 
-# class RCV(models.Model):
-#     rcv = models.CharField(max_length=20)
-
 class DataDate(models.Model):
     date = models.DateTimeField()
-
-class Customer(models.Model):
-    customer_code = models.IntegerField()
-
-# class ItemInfo(models.Model):
-#     # Permanent item info that is the same for all items of same time
-#     description = models.CharField(max_length=75)
-#     sku_name = models.CharField(max_length = 45)
-#     customer_code = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Item(models.Model):
     # Info that will change constantly
@@ -179,5 +167,7 @@ class Item(models.Model):
     inven_date = models.DateTimeField()
 
     data_date = models.ForeignKey(DataDate, on_delete=models.CASCADE)
-    # rcv = models.ForeignKey(RCV, on_delete=models.CASCADE)
-    # item_info = models.ForeignKey(ItemInfo, on_delete=models.CASCADE)
+    rcv = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    sku_name = models.CharField(max_length=50)
+    customer_code = models.IntegerField()
