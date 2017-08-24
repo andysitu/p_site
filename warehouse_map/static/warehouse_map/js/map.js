ddcd14897dc8eed4dfaa07554ec8e1eaa886c6ea
Andy Situ <and.situ@gmail.com>
2017-08-24 12:06:14 -0700

    move js func to map_ajax.js

--- warehouse_map/static/warehouse_map/js/map.js
+++ warehouse_map/static/warehouse_map/js/map.js
@@ -18,76 +18,6 @@ function ajax_map(location_arr, callback_funct) {
     });
 };
 
-function set_data_type() {
-    function create_options(){
-        var data_arr = null,
-            data_select = $( '#data-type-select' ),
-            data_arr_len;
-
-        data_arr = [
-            "item_ct", "Item Count",
-        ];
-        data_arr_len = data_arr.length/2;
-
-        for (i=0; i < data_arr_len; i++) {
-            data_select.append($("<option>",
-                {
-                    value: data_arr[i * 2],
-                    text: data_arr[i * 2 + 1],
-                }));
-        }
-    };
-    create_options()
-};
-
-function set_level_input(maxLevel) {
-    if (maxLevel == null)
-        maxLevel = 6;
-
-    function create_options(maxLevel){
-        var i;
-        var level_input = $( '#level-select' );
-        level_input.append($("<option>", {value: "All", text: "All",}));
-        for (i=1; i <= maxLevel; i++) {
-            level_input.append($("<option>", {value: i, text: i,}));
-        }
-    };
-
-    function increase_level(e){
-        var level_input = $( '#level-select' );
-        var level = level_input.val();
-        if (level == "All") {
-            level = 1;
-        } else {
-            level = parseInt(level);
-            level++;
-            if (level >= maxLevel){
-                level = maxLevel;
-            }
-        }
-        level_input.val(level);
-    };
-
-    function decrease_level(e){
-        var level_input = $( '#level-select' );
-        var level = level_input.val();
-        if (level == "1") {
-            level = 'All';
-        } else if (level == 'All'){
-            level == 'All';
-        } else {
-            level = parseInt(level);
-            level--;
-        }
-        level_input.val(level);
-    };
-    create_options(maxLevel)
-
-    $( '#minus-level' ).click(decrease_level);
-
-    $( '#plus-level' ).click(increase_level);
-};
-
 $( document ).ready(function() {
     // Setup canvas width, height, etc.
     (function set_canvas() {
@@ -229,6 +159,8 @@ function make_map(data_list) {
     } else {
         set_level_input();
         set_data_type();
+
+
         map_canvas_jobj.click(function(e){
             var clicked_y = e.offsetY,
                 clicked_x = e.offsetX,
