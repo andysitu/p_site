718d4d13170614161883d964ec80fb3ef49800db
Andy Situ <and.situ@gmail.com>
2017-08-28 15:08:08 -0700

    change map_data_list to map_data_arr

--- warehouse_map/static/warehouse_map/js/map.js
+++ warehouse_map/static/warehouse_map/js/map.js
@@ -12,8 +12,8 @@ function ajax_map(location_arr, callback_funct) {
             "loc[]": location_arr,
         },
         dataType: "json",
-        success: function(map_data_list) {
-            callback_funct(map_data_list);
+        success: function(map_data_arr) {
+            callback_funct(map_data_arr);
         },
     });
 };
@@ -35,8 +35,8 @@ $( document ).ready(function() {
 
     // Ajax to get grid_map (arrays)
     var loc_list = ['F', 'VC', 'S', 'P',];
-    ajax_map(loc_list, function(map_data_list) {
-            make_map(map_data_list);
+    ajax_map(loc_list, function(map_data_arr) {
+            make_map(map_data_arr);
         }
     );
 });
@@ -51,7 +51,7 @@ function remove_events() {
     $("#map-submit-button").off("click");
 };
 
-function make_map(map_data_list, loc) {
+function make_map(map_data_arr, loc) {
     var map_canvas_jobj = $( '#map_canvas' ),
         map_canvas = map_canvas_jobj[0],
         i,
@@ -69,10 +69,10 @@ function make_map(map_data_list, loc) {
     // Get total width & length of arrays
     var max_num_down = 0,
         total_num_across = 0,
-        data_length = map_data_list.length,
+        data_length = map_data_arr.length,
         num_down;
     for ( i = 0; i < data_length; i++) {
-        map_data_dic = map_data_list[i];
+        map_data_dic = map_data_arr[i];
         num_down = map_data_dic.num_down;
 
         if (num_down > max_num_down) {
@@ -90,7 +90,7 @@ function make_map(map_data_list, loc) {
 
     var start_x =0, start_y = 0;
     for ( i = 0; i < data_length; i++) {
-        map_data_dic = map_data_list[i];
+        map_data_dic = map_data_arr[i];
         image_map = map_data_dic["image_map"];
         map_info = draw_map(ctx, image_map, start_x, start_y, box_length);
 
@@ -105,7 +105,7 @@ function make_map(map_data_list, loc) {
 
     $("#map-submit-button").click(function(e){
         e.preventDefault();
-        map_search(map_data_list);
+        map_search(map_data_arr);
     });
 
     // Showing all locations
@@ -124,7 +124,7 @@ function make_map(map_data_list, loc) {
                 i, box_length;
 
             for (i = 0; i < data_length; i++) {
-                map_data_dic = map_data_list[i];
+                map_data_dic = map_data_arr[i];
                 box_length = map_data_dic.box_length;
                 if( clicked_x >= map_data_dic.start_x && clicked_x <= map_data_dic.end_x &&
                     clicked_y >= map_data_dic.start_y && clicked_y <= map_data_dic.end_y
@@ -157,7 +157,7 @@ function make_map(map_data_list, loc) {
                 i, box_length;
 
             for (i = 0; i < data_length; i++) {
-                map_data_dic = map_data_list[i];
+                map_data_dic = map_data_arr[i];
                 box_length = map_data_dic.box_length;
                 if( clicked_x >= map_data_dic.start_x && clicked_x <= map_data_dic.end_x &&
                     clicked_y >= map_data_dic.start_y && clicked_y <= map_data_dic.end_y
@@ -165,8 +165,8 @@ function make_map(map_data_list, loc) {
                 {
                     loc = map_data_dic.loc;
 
-                    ajax_map( [loc,] , function(map_data_list){
-                        make_map(map_data_list, loc);
+                    ajax_map( [loc,] , function(map_data_arr){
+                        make_map(map_data_arr, loc);
                     });
                     return 1;
                 }
@@ -185,7 +185,7 @@ function make_map(map_data_list, loc) {
                 i, box_length;
 
             for (i = 0; i < data_length; i++) {
-                map_data_dic = map_data_list[i];
+                map_data_dic = map_data_arr[i];
                 box_length = map_data_dic.box_length;
                 if( clicked_x >= map_data_dic.start_x && clicked_x <= map_data_dic.end_x &&
                     clicked_y >= map_data_dic.start_y && clicked_y <= map_data_dic.end_y)
