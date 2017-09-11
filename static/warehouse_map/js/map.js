b9f1ad6c91d79a7965149a22b8c133aae530e2c0
Andy Situ <and.situ@gmail.com>
2017-09-11 13:13:36 -0700

    forgot collectstatic again

--- static/warehouse_map/js/map.js
+++ static/warehouse_map/js/map.js
@@ -350,7 +350,7 @@ function click_map_for_info(e) {
             } else {
                 page_functions.display_loc_info(location);
             }
-            console.log(data);
+            console.log(location, map_data_dic["data_map"], data);
             this.highlight_map(i, x, y);
         } else {
             this.restore_canvas();
