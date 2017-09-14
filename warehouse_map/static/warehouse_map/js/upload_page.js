5b51d5db04df0f1d9ad4a2381d97c88df4083bf4
Andy Situ <and.situ@gmail.com>
2017-09-14 14:46:42 -0700

    upload page checks for correct file by js

--- warehouse_map/static/warehouse_map/js/upload_page.js
+++ warehouse_map/static/warehouse_map/js/upload_page.js
@@ -1,20 +1,47 @@
 $( document ).ready(function() {
     $( '#upload-button' ).click(function(e) {
+        var result = false;
         e.preventDefault();
 
-        check_excel_file();
-        
-        $( '#upload-button' ).attr('disabled', true);
-        $( '#id_excel_data_file' ).attr('disabled', true);
+        var loading_icon_html = "<i class='fa fa-refresh fa-spin fa-3x fa-fw' aria-hidden='true'></i>";
+
+        result = check_excel_file(e);
+
+        if (result) {
+            e.click();
+            $( '#upload-button' ).attr('disabled', true);
+            $( '#id_excel_data_file' ).attr('disabled', true);
+
+            write_upload_msg( gettext("Currently Uploading.") + loading_icon_html);
+        }
     });
 });
 
 function check_excel_file() {
     var files = $( '#id_excel_data_file' )[0].files,
         files_length = files.length,
-        file, i;
+        re = /(\d+).(\w+)/,
+        file, i, name,
+        re_result;
 
-    for (i = 0; i < files_length; i++) {
-        file = files[i];
+    if (files_length == 0) {
+        write_upload_msg(gettext("No file was uploaded."));
+        return false;
     }
+
+    file = files[0];
+    filename = file.name;
+    re_result = re.exec(filename);
+    if (re_result == null || re_result[1].length != 14) {
+        write_upload_msg(gettext("The file is named incorrectly. It should contain 14 numbers for year, month, day, hour, minute, and second."));
+        return false;
+    } else if (re_result[2] != 'xlsx') {
+        write_upload_msg("The file should be an excel file, ending in XLSX.");
+        return false;
+    }
+    return true;
+}
+
+function write_upload_msg(msg) {
+    $( '#upload-msg-div' ).html(msg);
 }
