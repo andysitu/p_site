8c928bcc121ee555c3a56053d924f0feda0d2be9
Andy Situ <and.situ@gmail.com>
2017-09-20 16:57:35 -0700

    menu_screen obj that contains functions that fill menu screen

--- static/warehouse_map/js/page_functions.js
+++ static/warehouse_map/js/page_functions.js
@@ -1,11 +1,23 @@
 /*
 For functions that edit everything except canvas, ie the map
+Contains page_functions [obj],
+           menu_screen [obj]
  */
 
 var page_functions = {
     div_underline_class: "underline",
     make_underline_div: function(text) {
         return "<div class='" + this.div_underline_class + "'>" + String(text) + "</div>";
+    },
+    make_menuScreen: function(menu_type) {
+        // Delete any existing menu screens
+        menu_screen.empty();
+
+        switch(menu_type) {
+            case "delete_date":
+                menu_screen.fill_delmenu();
+                break;
+        };
     },
     fill_sidemenu: function(max_level) {
     /**
@@ -23,34 +35,7 @@ var page_functions = {
         map_ajax.set_date_input();
         map_ajax.change_data_type_select();
     },
-    fill_delmenu: function() {
-        var $menu_container = $( '#menu-screen-container'),
-            $form_group = $("<form></form>"),
-            $date_select_div = $("<form>", {
-                id: "date-del-select-div",});
 
-        $("<label>", {
-            for: "date-del-select",
-            text: gettext('Date'),
-        })
-            .appendTo($date_select_div);
-        $("<select>", {
-            "class": "form-control",
-            id: "date-del-select",
-        })
-            .appendTo($date_select_div);
-
-        var $date_del_but = $("<button>", {
-            "type": "submit",
-            id: "date-del-submit",
-            "class": "btn btn-primary",
-            "text": gettext("Delete"),
-        });
-
-        $form_group.append($date_select_div);
-        $form_group.append($date_del_but);
-        $menu_container.append($form_group);
-    },
 
     display_loc_info: function(location, info_dic) {
         /**
@@ -109,5 +94,39 @@ var page_functions = {
             }
         }
         return msg
+    },
+};
+
+var menu_screen = {
+    empty: function() {
+        $( '#menu-screen-container').empty();
+    },
+    fill_delmenu: function() {
+        var $menu_container = $( '#menu-screen-container'),
+            $form_group = $("<form></form>"),
+            $date_select_div = $("<form>", {
+                id: "date-del-select-div",});
+
+        $("<label>", {
+            for: "date-del-select",
+            text: gettext('Date'),
+        })
+            .appendTo($date_select_div);
+        $("<select>", {
+            "class": "form-control",
+            id: "date-del-select",
+        })
+            .appendTo($date_select_div);
+
+        var $date_del_but = $("<button>", {
+            "type": "submit",
+            id: "date-del-submit",
+            "class": "btn btn-primary",
+            "text": gettext("Delete"),
+        });
+
+        $form_group.append($date_select_div);
+        $form_group.append($date_del_but);
+        $menu_container.append($form_group);
     },
 };
