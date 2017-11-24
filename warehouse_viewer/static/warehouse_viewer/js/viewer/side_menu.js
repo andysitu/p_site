ddf4c10c8d281210639cf8b32dafb354497e09a2
Andy Situ <and.situ@gmail.com>
2017-11-24 15:49:24 -0800

    move search function to viewer_processor

--- warehouse_viewer/static/warehouse_viewer/js/viewer/side_menu.js
+++ warehouse_viewer/static/warehouse_viewer/js/viewer/side_menu.js
@@ -36,37 +36,11 @@ var side_menu = {
         $submit_button.prop("disabled", false);
     },
     submit: function(e) {
-        var $form = $( e.target ),
-            data_array = $form.serializeArray(),
-            form_url = $form.attr("action"),
-            form_method = $form.attr("method");
-
-        var form_data = {},
-            i,
-            data_arr_len = data_array.length;
-
-        for (i = 0; i < data_arr_len; i++) {
-             var data_dic = data_array[i];
-             if (data_dic["name"] != "csrfmiddlewaretoken") {
-                 form_data[data_dic["name"]] = data_dic["value"];
-             }
-        }
-
-        var data_mode = form_data["mode"],
-            data_type = form_data['data-type'];
-
         this.make_submitButton_loading();
 
-        $.ajax({
-            // warehouse_viewer.search_ajax
-            url: form_url,
-            data: form_data,
-            method: form_method,
-            success: function(data) {
-                viewer.display(data_mode, data_type, data);
-                side_menu.renew_submitButton();
-            },
-        });
+        viewer_processor.submit_search(e, function() {
+            side_menu.renew_submitButton();
+        })
     },
 };
 
--- warehouse_viewer/static/warehouse_viewer/js/viewer/viewer_processor.js
+++ warehouse_viewer/static/warehouse_viewer/js/viewer/viewer_processor.js
@@ -1,3 +1,37 @@
+var viewer_processor = {
+    submit_search: function(form_element, callback_function) {
+        var $form = $( form_element.target ),
+            data_array = $form.serializeArray(),
+            form_url = $form.attr("action"),
+            form_method = $form.attr("method");
+
+        var form_data = {},
+            i,
+            data_arr_len = data_array.length;
+
+        for (i = 0; i < data_arr_len; i++) {
+             var data_dic = data_array[i];
+             if (data_dic["name"] != "csrfmiddlewaretoken") {
+                 form_data[data_dic["name"]] = data_dic["value"];
+             }
+        }
+
+        var data_mode = form_data["mode"],
+            data_type = form_data['data-type'];
+
+        $.ajax({
+            // warehouse_viewer.search_ajax
+            url: form_url,
+            data: form_data,
+            method: form_method,
+            success: function(data) {
+                viewer.display(data_mode, data_type, data);
+                callback_function();
+            },
+        });
+    },
+};
+
 var viewer_storage = {
     retrieve: function() {
 
