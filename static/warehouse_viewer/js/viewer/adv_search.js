e64355b62c446f3c50dfea2fdcb31e691482f5af
Andy Situ <and.situ@gmail.com>
2018-04-02 17:16:07 -0700

    change form in adv_search

--- static/warehouse_viewer/js/viewer/adv_search.js
+++ static/warehouse_viewer/js/viewer/adv_search.js
@@ -12,22 +12,23 @@ var formObj = {
         return $("#form-container");
     },
     add_date_input: function() {
-        var $disp_container = this.get_$form_container();
+        var $form_container = this.get_$form_container();
 
-        $disp_container.append( settings_maker.date_input_1() );
+        $form_container.append( settings_maker.date_input_1() );
     },
     add_filter_div: function() {
-        var $disp_container = this.get_$form_container();
+        var $form_container = this.get_$form_container();
 
-        $disp_container.append( settings_maker.filter_div() );
+        $form_container.append( settings_maker.filter_div() );
     },
     add_loc_level_select: function() {
-        var $loc_and_level = settings_maker.loc_and_level_container(),
-            $disp_container = this.get_$form_container();
+        var $loc_and_level_dic = settings_maker.loc_and_level_container(),
+            $form_container = this.get_$form_container();
 
-        $disp_container.append( $loc_and_level );
-    },
-    add_submit: function() {
+        var $loc_div = $loc_and_level_dic["$loc_div"],
+            $level_container = $loc_and_level_dic["$level_container"];
 
+        $form_container.append($loc_div);
+        $form_container.append($level_container);
     },
 };
