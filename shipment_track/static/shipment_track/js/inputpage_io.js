60b4c6bc6494d6d97709b1e3327c450e6cb66845
Andy Situ <and.situ@gmail.com>
2018-11-16 17:15:52 -0800

    slight changes to input

--- shipment_track/static/shipment_track/js/inputpage_io.js
+++ shipment_track/static/shipment_track/js/inputpage_io.js
@@ -53,36 +53,55 @@ class TypeInput {
     constructor() {
         this.id = "trackingNumInput";
         this.input_container_id = "type-input-div";
-        this.create_select();
+        // If addType_status is true, then add_type needs to be an
+        // input ele rather than select to add the type in.
+        this.addType_status = false;
         this.load();
+        
     }
 
     load(){
+        this.createAddTypeInput();
         this.typeAddButtonHandler()
+    }
+
+    switch_addType_status() {
+        if (this.addType_status)
+            this.addType_status = false;
+        else
+            this.addType_status = true; 
+    }
+    createAddTypeInput() {
+        /**
+         * Creates an input / select element for add type depending 
+         *  on addType_status. 
+         *  input - > select, true - > input element
+         */
+        if (this.addType_status) {
+            var input_container = this.get_input_container();
+            var domstring = '<input class="form-control" name="type" id="typeInput"></input>';
+            input_container.innerHTML = domstring;
+        } else {
+            var input_container = this.get_input_container();
+            var domstring = '<select class="form-control" name="type" id="typeSelect"></select>';
+            input_container.innerHTML = domstring;
+        }
     }
 
     typeAddButtonHandler() {
         var add_button = document.getElementById("add-type-button");
         var that = this;
         add_button.addEventListener("click", function(e){
-            that.create_input();
+            console.log(that);
+            that.switch_addType_status();
+            that.createAddTypeInput();
         });
     }
+
+
     
     get_input_container() {
         return document.getElementById(this.input_container_id);
-    }
-
-    create_select() {
-        var input_container = this.get_input_container();
-        var domstring = '<select class="form-control" name="type" id="typeSelect"></select>';
-        input_container.innerHTML = domstring;
-
-    }
-    create_input() {
-        var input_container = this.get_input_container();
-        var domstring = '<input class="form-control" name="type" id="typeInput"></input>';
-        input_container.innerHTML = domstring;
     }
 }
 
